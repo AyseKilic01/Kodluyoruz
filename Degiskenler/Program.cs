9abb4384ad6ce961c97ab6ab694e6a53434a4739
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-05-13 23:33:59 +0300

    variables

---
+++ Degiskenler/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Degiskenler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint ui = 2;
+            long l = 4;
+            ulong ul = 4;
+            int i = 123;
+            string name = "Ayse";
+            char ch = 'a';
+            float a = 12f;
+            double b = 123d;
+
+
+        }
+    }
+}
