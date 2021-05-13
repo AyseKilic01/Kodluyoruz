4677eaddff9b47fb172b09235e7051d717f7a04f
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-05-13 22:44:45 +0300

    basic

--- Kodluyoruz/Program.cs
+++ Kodluyoruz/Program.cs
@@ -10,6 +10,14 @@ namespace Kodluyoruz
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminizi Giriniz : ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soy isminizi Giriniz : ");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Adınız: " + name + " Soyadınız: " + surname);
+            Console.ReadLine();
+
         }
     }
 }
