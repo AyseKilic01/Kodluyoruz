9a1e9dec09c3bc58104e4988af7deabef4f6335e
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-05-20 21:34:22 +0300

    added Enums

---
+++ Enums/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int durum = Convert.ToInt32(Console.ReadLine());
+           // Console.WriteLine(Days.Friday);
+            if(durum > 20)
+            {
+                Console.WriteLine(" " + Days.Friday + " " + HavaDurum.Hot);
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+    enum Days
+    {
+        Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+    }
+    enum HavaDurum
+    {
+        Cold, Hot
+    }
+}
