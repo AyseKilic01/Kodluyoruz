826229a5710023dac5e7a42c65cd3e63f60f685d
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-05-20 21:01:59 +0300

    added statics

---
+++ Statics/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Statics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Ayse","Kilic");
+            Employee employee1 = new Employee("Ayse","Kilic");
+            Employee employee2 = new Employee("Ayse","Kilic");
+            Employee employee3 = new Employee("Ayse","Kilic");
+            Console.WriteLine("Çalışan Sayısı {0} ", Employee.EmployeeCount);
+            Console.ReadLine();
+        }
+    }
+    class Employee
+    {
+        private static int employeeCount;
+        public static int EmployeeCount { get => employeeCount; }
+
+        public string Name;
+        public string surName;
+
+        static Employee()
+        {
+            employeeCount = 0;
+        }
+
+        public Employee(string Name, string Surname)
+        {
+            this.Name = Name;
+            this.surName = Surname;
+            employeeCount++;
+
+        }
+
+
+    }
+}
