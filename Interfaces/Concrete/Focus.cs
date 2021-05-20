ba7622ce09334d54605df1b9ae6ae523c507ba58
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-05-20 22:12:30 +0300

    added Inteface examples

---
+++ Interfaces/Concrete/Focus.cs
@@ -0,0 +1,26 @@
+﻿using Interfaces.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces.Concrete
+{
+    class Focus : ICar
+    {
+        private string color;
+        public string MyColor { get=> color; set => color = value; }
+
+        public void colorCar(string color)
+        {
+            MyColor = color;
+            Console.WriteLine(this.MyColor);
+        }
+
+        public int wheelCount()
+        {
+            return 6;
+        }
+    }
+}
---
+++ Interfaces/Program.cs
@@ -0,0 +1,38 @@
+﻿using Interfaces.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmsLogger smsLogger = new SmsLogger();
+            smsLogger.MessageLogs();
+            string color = "Mavi";
+            Focus focus = new Focus();
+            focus.colorCar(color);
+            Console.WriteLine(" "  + focus.wheelCount()); 
+            Console.ReadLine();
+        }
+    }
+
+    class SmsLogger : ILogger
+    {
+        public void MessageLogs()
+        {
+            Console.WriteLine("Sms kaydedildi.");
+        }
+    }
+    class DatabaseLogger : ILogger
+    {
+        public void MessageLogs()
+        {
+            Console.WriteLine("Database e kaydedildi.");
+        }
+    }
+}
