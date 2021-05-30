dd126d6685cca5107e01f29a797cdf5ce780b15e
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-05-30 21:06:20 +0300

    added new project

---
+++ ToDoApp/Manage/ChooseClass.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Manage
+{
+    public class ChooseClass
+    {
+        public ChooseClass()
+        {
+            Choose();
+        }
+       public static void Choose()
+        {
+            Console.WriteLine("Lütfen yapmak işlemi seçiniz : ");
+            Console.WriteLine("********************************");
+            Console.WriteLine("(1) Board Listelemek");
+            Console.WriteLine("(2) Board'a Kart Eklemek");
+            Console.WriteLine("(3) Board'dan Kart Silmek");
+            Console.WriteLine("(4) Kart Taşımak");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    AddNew();
+                    break;
+                case 2:
+                    Delete();
+                    break;
+                case 3:
+                    Update();
+                    break;
+                case 4:
+                    List();
+                    break;
+            }
+        }
+
+        private static void List()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Update()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void Delete()
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void AddNew()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ToDoApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToDoApp.Manage;
+
+namespace ToDoApp
+{
+    public class Program
+    {
+        #region objects
+      
+        #endregion
+        static void Main(string[] args)
+        {
+         ChooseClass choose = new ChooseClass();
+            Console.ReadLine();
+        }
+    }
+}
